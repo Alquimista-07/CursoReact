ac4a49a33ae831556dc2e9e7557666c738a938ea
Alquimista-07 <jdca.07@gmail.com>
2024-02-01 11:32:20 -0500

    14. Variables y constantes

---
+++ 2. Seccion 3 - Introduccion a JavaScript moderno/02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,22 @@
+// NOTA: La documentación para crear el proyecto de react esta en:
+//       https://create-react-app.dev/
+
+// Variables y Constantes
+
+const nombre = 'Fernando';
+
+const apellido = 'Herrara';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+// var no se debe de usar...
+if( true ){
+    const nombre = 'Peter';
+
+    console.log(nombre);
+}
+
+console.log(valorDado);
