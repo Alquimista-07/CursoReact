f5642245a913edee795bf4d75a39a1c4846418c2
Alquimista-07 <jdca.07@gmail.com>
2024-02-04 13:13:36 -0500

    21. Import, export y funciones comunes de arreglos

---
+++ 2. Seccion 3 - Introduccion a JavaScript moderno/02-intro-javascript/src/bases/08-imp-exp.js
@@ -0,0 +1,27 @@
+// NOTA: La documentación para crear el proyecto de react esta en:
+//       https://create-react-app.dev/
+
+// Import, export y funciones comunes de arreglos
+// Enlace con la documentación Array.prototype.find(): https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+
+import { heroes } from '../data/heroes';
+
+// const getHeroeById = (id) => {
+//     return heroes.find((heroe) => {
+//         if(heroe.id === id) {
+//             return true;
+//         }
+//         else {
+//             return false;
+//         }
+//     });
+// }
+
+const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id );
+
+console.log(getHeroeById(2));
+
+// const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner.includes(owner) );
+const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner );
+
+console.log(getHeroesByOwner('Marvel'));
---
+++ 2. Seccion 3 - Introduccion a JavaScript moderno/02-intro-javascript/src/data/heroes.js
@@ -0,0 +1,30 @@
+// Import, export y funciones comunes de arreglos
+// Enlace con la documentación Array.prototype.find(): https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+
+export const heroes = [
+    {
+        id: 1,
+        name: 'Batman',
+        owner: 'DC'
+    },
+    {
+        id: 2,
+        name: 'Spiderman',
+        owner: 'Marvel'
+    },
+    {
+        id: 3,
+        name: 'Superman',
+        owner: 'DC'
+    },
+    {
+        id: 4,
+        name: 'Flash',
+        owner: 'DC'
+    },
+    {
+        id: 5,
+        name: 'Wolverine',
+        owner: 'Marvel'
+    },
+];
