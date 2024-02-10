a0b15c757f853d98e2d8e23f42bedfec99720c81
Alquimista-07 <jdca.07@gmail.com>
2024-02-10 11:30:16 -0500

    38. Retornar elementos en el Componente - Fragment

--- 3. Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/FirstApp.jsx
+++ 3. Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/FirstApp.jsx
@@ -1,7 +1,19 @@
+// import { Fragment } from "react"
 
 //* 37. Tarea - Crear un nuevo componente
 export const FirstApp = () => {
   return (
-    <h1>Ariadna</h1>
+    // NOTA: Recordemos que en React siempre debe existir un nodo padre que contenga a los demás elementos, que puede ser un div o un Fragment
+    //       y el cual se representa con <Fragment><Fragment/> y se debe de importar o también recordemos que podemos usar solo su sinónimo que es
+    //       <></> y el cual si no es necesario importarlo y básicamente de la misma forma representa el Fragment.
+    //       Ahora estos Fragment los usamos cuando queremos colocar un bloque de elementos JSX que tenga no tengan estilos o que solo algunos de esos
+    //       elementos tengan estilos css y no todos.
+   
+    // <Fragment>
+    <>
+        <h1>Ariadna</h1>
+        <p>Spy un subtitulo</p>
+    </>
+    // </Fragment>
   )
 }
