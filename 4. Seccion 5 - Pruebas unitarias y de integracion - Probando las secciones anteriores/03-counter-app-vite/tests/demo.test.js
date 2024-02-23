34182af204edd7cde86da653a0ee5e5dea48bf29
Alquimista-07 <jdca.07@gmail.com>
2024-02-23 16:31:17 -0500

    54. Jest - Expect - toBe

--- 4. Seccion 5 - Pruebas unitarias y de integracion - Probando las secciones anteriores/03-counter-app-vite/tests/demo.test.js
+++ 4. Seccion 5 - Pruebas unitarias y de integracion - Probando las secciones anteriores/03-counter-app-vite/tests/demo.test.js
@@ -2,9 +2,29 @@
 //       https://jestjs.io/docs/getting-started
 // NOTA: Para crear la prueba debemos usar el método test() dentro del cual vamos a poner una descripción
 //       y luego le colocamos un callback
-test('Esta prueba no debe de fallar', () => {
-    // NOTA: Acá la idea es hacer las asserciones
-    if(0 === 1) {
-        throw new Error('No puede dividir entre cero');
-    }
+
+// Agreganmos un agrupador
+describe('Pruebas en <DemoComponent />', () => {
+
+    test('Esta prueba no debe de fallar', () => {
+        // NOTA: Acá la idea es hacer las asserciones
+        
+        // NOTA: Hay tres pasos para las pruebas pero no necesariamente deben estar los tres
+        // 1. Inicialización
+        // 2. Estímulo -> Lo que aplicamos al sujeto de pruebas
+        // 3. Aserciones -> Observar el comportamiento... esperado.
+    
+        // 1. Inicialización
+        const message1 = 'Hola Mundo';
+    
+        // 2. Estímulo
+        const message2 = message1.trim();
+    
+        // 3. Aserciones
+        // En este caso ya no usamos el if para comparar que los mensajes sean iguales ya que JEST nos probee de métodos para esto
+        // Enlace documentación expect: https://jestjs.io/docs/expect
+        expect( message1 ).toBe(message2);
+    
+    });
+    
 });
