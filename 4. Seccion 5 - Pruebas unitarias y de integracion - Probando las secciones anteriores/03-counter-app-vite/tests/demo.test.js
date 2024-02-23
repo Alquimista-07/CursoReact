03f84c91d16ca937a7c44be7702adaf8dfd87b12
Alquimista-07 <jdca.07@gmail.com>
2024-02-23 10:30:27 -0500

    53. Mi primera prueba y configuraciones iniciales

---
+++ 4. Seccion 5 - Pruebas unitarias y de integracion - Probando las secciones anteriores/03-counter-app-vite/tests/demo.test.js
@@ -0,0 +1,8 @@
+// NOTA: Para crear la prueba debemos usar el método test() dentro del cual vamos a poner una descripción
+//       y luego le colocamos un callback
+test('Esta prueba no debe de fallar', () => {
+    // NOTA: Acá la idea es hacer las asserciones
+    if(0 === 1) {
+        throw new Error('No puede dividir entre cero');
+    }
+});
